99873b81853f0c7bca71bcaf6186dfbdced13ca6
TayabHussain <tayabhussain@live.nl>
2023-05-13 03:15:26 +0200

    added login action file

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+require "config.php";
+?>
 <!doctype html>
 <html lang="en">
 
@@ -17,19 +20,20 @@
                 <div class="col-12 col-md-8 col-lg-6 col-xl-5">
                     <div class="card shadow-2-strong border-dark" style="border-radius: 1rem; border-width: thick;">
                         <div class="card-body p-5 text-center">
-
                             <h3 class="mb-5">Sign in</h3>
-
-                            <div class="form-outline mb-4">
-                                <input type="email" id="typeEmailX-2" class="form-control form-control-lg" />
-                                <label class="form-label" for="typeEmailX-2">Email</label>
-                            </div>
-
-                            <div class="form-outline mb-4">
-                                <input type="password" id="typePasswordX-2" class="form-control form-control-lg" />
-                                <label class="form-label" for="typePasswordX-2">Password</label>
-                            </div>
-                            <button class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
+                            <!-- Login Form -->
+                            <form action="php/login_action.php" method="post">
+                                <div class="form-outline mb-4">
+                                    <label class="form-label" for="typeEmailX-2">Email:</label>
+                                    <input type="email" name="email" class="form-control form-control-lg"
+                                        placeholder="example@example.com">
+                                </div>
+                                <div class="form-outline mb-4">
+                                    <label class="form-label" for="typePasswordX-2">Password:</label>
+                                    <input type="password" name="password" class="form-control form-control-lg">
+                                </div>
+                                <button class="btn btn-primary btn-lg btn-block" type="submit">Login</button>
+                            </form>
                         </div>
                     </div>
                 </div>
